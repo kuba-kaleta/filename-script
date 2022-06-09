09e9ee2b1113f53f278044cbe4537d24156828dc
JakubKKK <kubamen9@wp.pl>
2022-06-09 17:57:29 +0200

    basic filename with date taken

--- filename.ps1
+++ filename.ps1
@@ -1,1 +1,11 @@
-Write-Host "Congratulations! Your first script executed successfully"
+# Write-Host "Congratulations! Your first script executed successfully"
+
+$FOlder = 'C:\Users\kubam\Desktop\New'
+$CharWhiteList = '[^: \w\/]'
+$Shell = New-Object -ComObject shell.application
+$i = 1
+Get-ChildItem $FOlder *.jpg -Recurse  | ForEach-Object{
+    $dir = $Shell.Namespace($_.DirectoryName)
+    $DateTaken = [datetime]::ParseExact(($dir.GetDetailsOf($dir.ParseName($_.Name),12) -replace $CharWhiteList),"yyyyMMdd HH:mm",$Null)
+    Rename-Item $_.FullName ('{0:yyyy_MM_dd}_Nr{1:0000}.jpg' -f $DateTaken, $i++)
+}
