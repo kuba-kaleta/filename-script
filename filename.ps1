a71f5ae6137124c1d23d4c30c9dffda97a1a28a4
JakubKKK <kubamen9@wp.pl>
2022-06-17 18:15:37 +0200

    file comments and file size

--- filename.ps1
+++ filename.ps1
@@ -1,11 +1,12 @@
 # Change names of all photos in a folder to earliest aviable date
 
 $FOlder = 'C:\Users\kubam\Desktop\New'
+$types = ('.jpg', '.jpeg', '.heic')
 $CharWhiteList = '[^: \w\/]'
 $Shell = New-Object -ComObject shell.application
 
 $i = 1
-Get-ChildItem $FOlder *.jpg -Recurse | ForEach-Object{
+Get-ChildItem $FOlder -Recurse | where {$_.extension -in $types} | ForEach-Object{ # -Recurse
     $dir = $Shell.Namespace($_.DirectoryName)
     $strModified = $dir.GetDetailsOf($dir.ParseName($_.Name),3)
     $strCreated = $dir.GetDetailsOf($dir.ParseName($_.Name),4)
@@ -32,21 +33,34 @@ Get-ChildItem $FOlder *.jpg -Recurse | ForEach-Object{
         $list.Add($DateTaken)
     }
 
-    $list_sort = $list | Sort-Object
-    $list_dupl = Get-ChildItem $_.DirectoryName *.jpg
-    $earliestDate = $list_sort[0]
-    $short_name = '{0:yyyy_MM_dd_HH}h{1:mm}.jpg' -f $earliestDate, $EarliestDate
+    $size = $_.Length
+    If     ($size -gt 1TB) {$size_format = [string]::Format("{0:0.00} TB", $size / 1TB)}
+    ElseIf ($size -gt 1GB) {$size_format = [string]::Format("{0:0.00} GB", $size / 1GB)}
+    ElseIf ($size -gt 1MB) {$size_format = [string]::Format("{0:0.00} MB", $size / 1MB)}
+    ElseIf ($size -gt 1KB) {$size_format = [string]::Format("{0:0.00} kB", $size / 1KB)}
+    ElseIf ($size -gt 0)   {$size_format = [string]::Format("{0:0.00} B", $size)}
+    Else                   {$size_format = ""}
 
-    Write-Host " "
-    Write-Host $_.Name $short_name
+    $list_sort = $list | Sort-Object
+    $list_dupl = Get-ChildItem $_.DirectoryName | where {$_.extension -in $types}
+    $earliestDate = $list_sort[0]
+    $ext = $_.extension
+    $short_name = "{0:yyyy_MM_dd_HH}h{1:mm_}$size_format$ext" -f $earliestDate, $EarliestDate
+
+    # $comm = $dir.GetDetailsOf($dir.ParseName($_.Name), 24)
+    # if($comm.Length -ge 1001){
+    #     $comm = $comm.Substring(0, 999)
+    # }
+    # $dir.SetDetailsOf($dir.ParseName($_.Name), 24, "$comm ... $_.Name")
+    # Write-Host $_.Name $short_name
 
     foreach ($item in $list_dupl){
         if($item.Name -ne $_.Name){ # Substring(0, 16)
-            Write-Host $item.Name
+            # Write-Host $item.Name
             $list_dupl_w.Add($item)
         }
         else{
-            Write-Host "Odrzucony" $item.Name
+            # Write-Host "Odrzucony" $item.Name
         }
     }
 
@@ -54,7 +68,7 @@ Get-ChildItem $FOlder *.jpg -Recurse | ForEach-Object{
     foreach ($item in $list_dupl_w){
         if($item.Length -ge 16){
             if($item.Substring(0, 16) -eq $short_name.Substring(0, 16)){
-                Write-Host "false" $item
+                # Write-Host "false" $item
                 $short = $false
             }
         }        
@@ -64,10 +78,10 @@ Get-ChildItem $FOlder *.jpg -Recurse | ForEach-Object{
         $global:dupl = $true
         while($dupl){
             $dupl = $false
-            $long_name = '{0:yyyy_MM_dd_HH}h{1:mm}_{2:00000}.jpg' -f $EarliestDate, $EarliestDate, $i++
+            $long_name = "{0:yyyy_MM_dd_HH}h{1:mm}_{2:00000}$ext" -f $EarliestDate, $EarliestDate, $i++
             foreach ($item in $list_dupl_w){
                 if($long_name -eq $item){
-                    Write-Host "dupl"
+                    # Write-Host "dupl"
                     $dupl = $true
                 }
             }
